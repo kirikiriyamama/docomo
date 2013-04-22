249c07f6416cfe26f5b528006ebee4df7701b71b
remain <ponde.ponde.pond@gmail.com>
2013-04-22 19:43:06 +0900

    HTMLのパースを出来るようにしました

--- docomo.rb
+++ docomo.rb
@@ -8,3 +8,6 @@ require 'pp'
 
 url = 'http://www.nttdocomo.co.jp/support/utilization/product_update/list/index.html?s=date'
 doc = Nokogiri::HTML(open url)
+doc.xpath('//table').first.xpath('tr').each do |tr|
+	pp tr.text.split("\n")
+end
