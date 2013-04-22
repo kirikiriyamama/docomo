660ace9274d31e9b7f37e13954b65a460fc00024
remain <ponde.ponde.ponde@gmail.com>
2013-04-23 03:33:24 +0900

    差分のみをDBに記録出来るようにしました

--- docomo.rb
+++ docomo.rb
@@ -9,8 +9,6 @@ require 'pp'
 require File.join(__dir__, 'lib', 'Hash')
 
 
-db = Sequel.connect(YAML.load_file(File.join(__dir__, 'config', 'database.yml')).symbolize_keys)
-
 url = 'http://www.nttdocomo.co.jp/support/utilization/product_update/list/index.html?s=date'
 doc = Nokogiri::HTML(open url)
 
@@ -21,7 +19,29 @@ doc.xpath('//table').first.xpath('tr').each do |tr|
 end
 trs.shift
 
-trs.reverse.each do |tr|
+
+acquired_index = trs.size
+
+db = Sequel.connect(YAML.load_file(File.join(__dir__, 'config', 'database.yml')).symbolize_keys)
+unless db[:product_update].count.zero?
+	db_last_data = db[:product_update].order(:id).last
+	db_last_data.delete(:id)
+	db_last_data.delete(:acquisition_date)
+
+	trs.each_with_index do |tr, index|
+		web_data = tr.dup
+		web_data[:start_date] = Date.strptime(web_data[:start_date], '%Y年%m月%d日')
+		web_data[:end_date] = Date.strptime(web_data[:end_date], '%Y年%m月%d日まで') rescue web_data[:end_date] = nil
+
+		if db_last_data == web_data
+			acquired_index = index
+			break
+		end
+	end
+end
+
+
+trs.slice(0, acquired_index).reverse.each do |tr|
 	start_date = Date.strptime(tr[:start_date], '%Y年%m月%d日').strftime('%Y-%m-%d')
 	end_date = Date.strptime(tr[:end_date], '%Y年%m月%d日まで').strftime('%Y-%m-%d') rescue end_date = 0
 
